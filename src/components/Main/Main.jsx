33d3efd179d27b7155d4d7bb03bfed2295387885
unknown <wiliamfeng@gmail.com>
2021-01-30 10:03:05 +0700

    initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardHeader, Divider, Grid, Typography } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { ExpenseTrackerContext } from '../../context/context';
+import InfoCard from '../InfoCard';
+import Form from './Form/Form';
+import List from './List/List';
+import useStyles from './styles';
+
+const ExpenseTracker = () => {
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+          <InfoCard />
+        </Typography>
+        <Divider className={classes.divider} />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ExpenseTracker;
